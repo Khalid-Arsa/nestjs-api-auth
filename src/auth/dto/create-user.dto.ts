a3d6d7068c7de54f9e64eb725316a05a53a439f6
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-03-10 11:20:48 +0800

    wip

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, IsEmail, IsNotEmpty } from "class-validator"
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
